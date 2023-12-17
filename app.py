3f78cfeb61080ef761edf36255361baf68f0a546
Vaishnavi0805 <sarmalkar.vaishnavi2003@gmail.com>
2023-12-17 23:26:41 +0530

    ui updated

--- app.py
+++ app.py
@@ -1,93 +1,105 @@
-from dotenv import load_dotenv
-import os
-import google.generativeai as genai
-from io import BytesIO
-from llama_index.multi_modal_llms.gemini import GeminiMultiModal
-from PIL import Image
-import requests
-from io import BytesIO
-import matplotlib.pyplot as plt
-from llama_index.multi_modal_llms.generic_utils import (
-    load_image_urls,
-)
-load_dotenv()
-
-api_key = os.getenv("GOOGLE_API_KEY")
 import streamlit as st
 from PIL import Image
 import ast
-# Mock function to get dish information
-def get_dish_info(num_people):
-    image_urls = [
-    "https://images.slurrp.com/prodarticles/5iyqpeyd4ec.webp",
-    # Add yours here!
-    ]
-    image_documents = load_image_urls(image_urls)
+import google.generativeai as genai
+import os
+import json
 
-    gemini_pro = GeminiMultiModal(model_name="models/gemini-pro-vision")
-    img_response = requests.get(image_urls[0])
-    print(image_urls[0])
-    img = Image.open(BytesIO(img_response.content))
-    plt.imshow(img)
-    vision_model = genai.GenerativeModel('gemini-pro-vision')
-    a=genai.GenerationConfig(temperature=0.1,
-        max_output_tokens=2048)
-    response = vision_model.generate_content(["""Please generate a list of the dish name,a list for the ingredients required to prepare it,adjust the ingredients for serving size for 60 people, and the nutritional value of the ingredients.
-                                                Additionally, please provide a list for detailed instructions to prepare the recipe step by step as follows :
-                                                Step 1:
-                                                Step 2:
-                                                Step n:
+# Set Google API key
+os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
-                                                return the final response in below format and keep in mind to use measurements for protien - in grams, fats - in grams, calories - in calories and Quanity as required by the recipe given :
-                                                {
-                                                    'name':['Name of the dish'],
-                                                    'servig size':['Serving size mentioned']
-                                                    'nutritional value and ingredients': {'ing1:['Quantity:','protien:', 'carbs:','fats:','calories:']',
-                                                                                        'ing2:['Quantity:','protien:', 'carbs:','fats:','calories:']',
-                                                                                        'ing3:['Quantity:','protien:', 'carbs:','fats:,calories:']'},
-                                                    'Recipe steps':[Step 1:
-                                                                    Step 2:
-                                                                    Step n:]
-                                                }""",img],generation_config =a)
+# Load the Gemini model
+vision_model = genai.GenerativeModel('gemini-pro-vision')
+
+# Define custom colors and fonts
+primaryColor = "#F7CAC9"
+secondaryColor = "#FADEAD"
+accentColor = "#FFAC81"
+fontFamily = "Raleway"
+
+def generate_recipe_data(image, serving_size):
+    generation_config = genai.GenerationConfig(temperature=0.1, max_output_tokens=2048)
+    response = vision_model.generate_content(["""Please generate a list of the dish name,a list for the ingredients required to prepare it,adjust the ingredients for serving size for {serving_size} people, and the nutritional value of the ingredients.
+        Additionally, please provide a list for detailed instructions to prepare the recipe step by step as follows :
+        Step 1:
+        Step 2:
+        Step n:
+
+        return the final response in below format and keep in mind to use measurements for protein - in grams, fats - in grams, calories - in calories and Quantity as required by the recipe given :
+        {
+            'name':['Name of the dish'],
+            'serving size':['Serving size mentioned']
+            'nutritional value and ingredients': {'ing1':['Quantity:','protein:', 'carbs:','fats:','calories:']',
+                                                   'ing2':['Quantity:','protein:', 'carbs:','fats:,calories:']',
+                                                   'ing3':['Quantity:','protein:', 'carbs:','fats:,calories:']},
+            'Recipe steps':[Step 1:
+                            Step 2:
+                            Step n:]
+        } Very important: Make sure that your response starts from { and end with a }""", image], generation_config=generation_config)
     response.resolve()
-    op1=response.text
-    response_string = op1.replace("```", "\"\"\"")
-    recipe_data = ast.literal_eval(response_string)
-    print (recipe_data)
+    op1 = response.text
+    print (op1)
+    return op1
 
+# Streamlit app
+st.set_page_config(layout="wide", page_title="Recipe Magic", page_icon="🪄")
 
+# Hero section
+with st.container():
+    st.markdown(f"<h1 style='font-family: {fontFamily}; color: {primaryColor}'>NomNomNerd ༼ つ ◕_◕ ༽つ🍰🍔🍕</h1>", unsafe_allow_html=True)
+    st.image("https://freedesignfile.com/upload/2017/05/Delicious-sushi-on-a-black-background-Stock-Photo-06.jpg", width=800)
 
-    # return "Spaghetti Bolognese", ["Pasta", "Tomato Sauce", "Beef", "Onion"], {"Calories": 500, "Protein": 20, "Carbs": 60}, "Cook the pasta, brown the beef, mix with sauce, and serve."
+# Image upload and serving size section
+with st.container():
+    st.markdown("<style>div{text-align:center;}</style>", unsafe_allow_html=True)
+    st.markdown("---")
+    st.markdown(f"<h3 style='font-family: {fontFamily}; color: {primaryColor}'>Upload your food picture:</h3>", unsafe_allow_html=True)
+    uploaded_image = st.file_uploader("", type=["jpg", "jpeg", "png"])
+    st.markdown(f"<p style='font-family: {fontFamily}; color: {primaryColor}'>Number of people eating:</p>", unsafe_allow_html=True)
+    serving_size = st.number_input("", min_value=1, value=1)
 
-def main():
-    st.title("Dish Information App")
+# Display uploaded image and recipe generation button
+with st.container():
+    if uploaded_image is not None:
+        # Display uploaded image
+        image = Image.open(uploaded_image)
+        st.image(image, caption="Uploaded Image", width=400)
 
-    # Upload image
-    uploaded_file = st.file_uploader("Choose an image of the dish", type=["jpg", "jpeg", "png"])
+        # Recipe generation button
+        button_html = f"<button style='background-color: {accentColor}; color: white; font-family: {fontFamily}'>Get Cooking!</button>"
+        if st.markdown(button_html, unsafe_allow_html=True):
 
-    # Get user input for the number of people
-    num_people = st.text_input("Number of People to Prepare For", type="default")
-    try:
-        num_people = int(num_people)
-    except ValueError:
-        num_people = None
-    
-    # Display dish information only if both image and number of people are provided
-    if uploaded_file is not None and num_people is not None:
-        st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+            recipe_data = None
+            while not isinstance(recipe_data, dict):
+                recipe_data_str = generate_recipe_data(image, serving_size)
+                recipe_data_str = recipe_data_str.replace("```", "\"\"\"")
+                recipe_data_str = recipe_data_str.strip()
+                recipe_data = ast.literal_eval(recipe_data_str)
 
-        # Get dish information
-        dish_name, ingredients, nutritional_values, recipe = get_dish_info( num_people)
+            # Display recipe data
+            with st.container():
+                st.markdown("<style>div{text-align:center;}</style>", unsafe_allow_html=True)
+                st.markdown("---")
 
-        # Display dish information
-        st.subheader("Dish Information")
-        st.write(f"**Dish Name:** {dish_name}")
-        st.write(f"**Ingredients:** {', '.join(ingredients)}")
-        st.write("**Nutritional Values:**")
-        for nutrient, value in nutritional_values.items():
-            st.write(f"- {nutrient}: {value}g")
-        st.subheader("Recipe")
-        st.write(recipe)
+                # Display recipe name with formatting
+                st.markdown(f"<h2 style='font-family: {fontFamily}; color: {primaryColor}'>Delicious {recipe_data['name'][0]}</h2>", unsafe_allow_html=True)
 
-if __name__ == "__main__":
-    main()
+                # Display serving size
+                st.write(f"Serving size: {recipe_data['serving size'][0]} people", font_family=fontFamily)
+
+                # Display nutritional value and ingredients
+                st.markdown(f"<h3 style='font-family: {fontFamily}; color: {primaryColor}'>Ingredients and Nutrition:</h3>", unsafe_allow_html=True)
+
+                for ingredient, values in recipe_data["nutritional value and ingredients"].items():
+                    # Display ingredient name and quantity
+                    st.write(f"- **{ingredient}**: {values[0]}", font_family=fontFamily)
+
+                    # Create string with nutritional values using string formatting
+                    nutritional_info = f"\tProtein: {values[1]} grams, Carbs: {values[2]} grams, Fats: {values[3]} grams, Calories: {values[4]} calories"
+                    st.write(nutritional_info, font_family=fontFamily)
+
+                # Display recipe steps
+                st.markdown(f"<h3 style='font-family: {fontFamily}; color: {primaryColor}'>Steps:</h3>", unsafe_allow_html=True)
+                for i, step in enumerate(recipe_data["Recipe steps"]):
+                    st.write(f"- Step {i + 1}: {step}", font_family=fontFamily)
